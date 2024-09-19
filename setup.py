7ebebdf48e1bfb987ab11ca166f57b225f389c45
Kevork <kevork@superannotate.com>
2024-09-19 16:35:49 +0400

    pls

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='paa_test',  # Name of your package
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[],  # Add any dependencies here if needed
+)
